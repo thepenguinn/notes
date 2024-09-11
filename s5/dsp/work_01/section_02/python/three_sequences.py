a2ce6c8bde12cc8e133ba57118b56a977def5a51
Daniel V Mathew <danielvmathew27@gmail.com>
2024-09-12 05:00:42 +0530

    dsp: updated the three frequncies in w01s02

--- s5/dsp/work_01/section_02/python/three_sequences.py
+++ s5/dsp/work_01/section_02/python/three_sequences.py
@@ -1,12 +1,12 @@
 import numpy as np
 import pandas as pd
 
-F = 28
-t = np.linspace(0, 1.25 * np.pi / 4, 2500)
+X = 28
+t = np.linspace(0, np.pi / 4, 2500)
 
-x1 = np.sin( 2 * np.pi * F * t)
-x2 = np.sin( 2 * np.pi * (F + 0.1) * t)
-x3 = np.sin( 2 * np.pi * 2 * F * t)
+x1 = np.sin( 2 * np.pi * X * t)
+x2 = np.sin( 2 * np.pi * (2 * X) * t)
+x3 = np.sin( 2 * np.pi * ((2 * X) + 0.1) * t)
 
 data = pd.DataFrame(
     {
